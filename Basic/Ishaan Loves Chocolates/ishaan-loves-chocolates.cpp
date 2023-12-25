895d0ee44b34668ed7b14eb93ac0f92eb710f525
Saransh Bangar <saranshbangad@gmail.com>
2023-12-25 22:07:58 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Ishaan Loves Chocolates/ishaan-loves-chocolates.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int chocolates(int n, vector<int> &arr)
+    {
+        // code here
+        int ans=INT_MAX;
+        for (int i=0;i<arr.size();i++)
+        {
+            if (ans>arr[i])
+                ans=arr[i];
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        Solution obj;
+        int res = obj.chocolates(n, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
