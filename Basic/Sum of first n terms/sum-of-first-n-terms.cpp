03b4e7974a1a1d983994ac6f7723f66ceb071eef
Saransh Bangar <saranshbangad@gmail.com>
2023-12-25 22:10:32 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Sum of first n terms/sum-of-first-n-terms.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    long long sumOfSeries(long long N)
+    {
+        long long ans=((N*(N+1))/2)*((N*(N+1))/2);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.sumOfSeries(N) << "\n";
+    }
+}
+// } Driver Code Ends
