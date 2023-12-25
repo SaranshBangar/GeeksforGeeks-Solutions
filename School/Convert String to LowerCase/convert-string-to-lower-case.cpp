502562c78b01910713022b0d7b571fb0e6d595f2
Saransh Bangar <saranshbangad@gmail.com>
2023-12-25 21:42:15 +0530

    Added Solution - GfG to GitHub

---
+++ School/Convert String to LowerCase/convert-string-to-lower-case.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string toLower(string S)
+    {
+        for (int i=0;i<S.length();i++)
+        {
+            if (S[i]>='A' && S[i]<='Z')
+                S[i]+=32;
+        }
+        return S;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.toLower(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
