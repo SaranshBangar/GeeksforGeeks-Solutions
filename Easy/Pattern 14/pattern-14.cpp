bd6e88edb07eb1e65abc9e13acb854de22ee1e3d
Saransh Bangar <saranshbangad@gmail.com>
2023-12-26 01:23:10 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 14/pattern-14.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n)
+    {
+        for (int i=1;i<=n;i++)
+        {
+            char c='A';
+            for (int j=1;j<=i;j++)
+            {
+                cout<<c;
+                c++;
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
