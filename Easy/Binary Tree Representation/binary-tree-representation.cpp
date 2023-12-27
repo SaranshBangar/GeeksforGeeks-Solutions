1535926d03cfc9fd97e0dd1bfc428946fd62312e
Saransh Bangar <saranshbangad@gmail.com>
2023-12-27 16:00:32 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Binary Tree Representation/binary-tree-representation.cpp
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node {
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+struct node *newNode(int data) {
+  struct node *node = (struct node *)malloc(sizeof(struct node));
+
+  node->data = data;
+
+  node->left = NULL;
+  node->right = NULL;
+  return (node);
+}
+
+void traverseInOrder(struct node *temp, vector<int> &inorder) {
+
+  if (temp != NULL) {
+    traverseInOrder(temp->left,inorder);
+    // cout << " " << temp->data;
+    inorder.push_back(temp->data);
+    traverseInOrder(temp->right,inorder);
+  }
+  return;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*struct node {
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+struct node *newNode(int data) {
+  struct node *node = (struct node *)malloc(sizeof(struct node));
+
+  node->data = data;
+
+  node->left = NULL;
+  node->right = NULL;
+  return (node);
+}*/
+
+class Solution{
+public:
+
+    void create_tree(node* root0, vector<int> &vec)
+    {
+        if (vec.empty() || root0==NULL)
+            return;
+        queue<node*>q;
+        q.push(root0);
+        for (int i=1; i<vec.size(); i+=2)
+        {
+            node* current=q.front();
+            q.pop();
+            if (vec[i]!=-1)
+            {
+                current->left=newNode(vec[i]);
+                q.push(current->left);
+            }
+            if (i+1<vec.size() && vec[i+1]!=-1)
+            {
+                current->right = newNode(vec[i+1]);
+                q.push(current->right);
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while(t--){
+        //Input
+        vector<int> vec(7);
+        for(int i = 0;i<7;i++){
+            cin >> vec[i];
+        }
+        struct node *root0 = newNode(vec[0]);
+        root0->left = newNode(vec[1]);
+        root0->right = newNode(vec[2]);
+        root0->left->left = newNode(vec[3]);
+        root0->left->right = newNode(vec[4]);
+        root0->right->left = newNode(vec[5]);
+        root0->right->right = newNode(vec[6]);
+
+        Solution obj;
+        struct node *root = newNode(vec[0]);
+        obj.create_tree(root, vec);
+
+        vector<int> a;
+        traverseInOrder(root0,a);
+        vector<int> b;
+        traverseInOrder(root,b);
+        if(a == b) cout << 1 << endl;
+        else cout << -1 << endl;
+
+       
+        
+
+        
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+
+// } Driver Code Ends
