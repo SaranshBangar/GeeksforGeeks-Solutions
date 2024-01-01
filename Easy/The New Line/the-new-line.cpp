d72da99c1e5bdb84ed6376f38651b2592f480277
Saransh Bangar <saranshbangad@gmail.com>
2024-01-01 16:20:09 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/The New Line/the-new-line.cpp
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<iostream>
+using namespace std;
+
+void printNewLine();
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// Function to print each word in new line
+void printNewLine()
+{
+    cout<<"Geeks"<<endl<<"for"<<endl<<"Geeks";
+}
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    //function call
+    printNewLine();
+    
+}
+
+// } Driver Code Ends
