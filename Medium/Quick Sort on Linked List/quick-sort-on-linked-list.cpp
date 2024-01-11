9c7d549d4036e6acd7b5e9a3b01515a630630c62
Saransh Bangar <saranshbangad@gmail.com>
2024-01-11 09:54:31 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Quick Sort on Linked List/quick-sort-on-linked-list.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <cstdio>
+using namespace std;
+ 
+/* a node of the singly linked list */
+struct node
+{
+    int data;
+    struct node *next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+/* A utility function to insert a node at the beginning of linked list */
+void push(struct node** head_ref, int new_data)
+{
+    /* allocate node */
+    struct node* new_node = new node(new_data);
+ 
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);
+ 
+    /* move the head to point to the new node */
+    (*head_ref)    = new_node;
+}
+ 
+/* A utility function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+void quickSort(struct node **headRef);
+int main()
+{
+	int t,n,x;
+	cin>>t;
+	while(t--){
+		
+        
+        cin>>n;n=n-1;
+		cin>>x;
+        node *temp,*head = new node(x);
+        temp=head;
+        while(n--)
+        {
+			cin>>x;
+          temp->next=new node(x);
+          temp=temp->next;
+			}
+    
+    quickSort(&head);
+ 
+    printList(head);
+ while(head!= NULL){
+     temp=head;
+     head=head->next;
+     free(temp);
+     
+ }
+	}
+    return 0;
+}
+// } Driver Code Ends
+
+
+ 
+/* a node of the singly linked list 
+struct node
+{
+    int data;
+    struct node *next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}; */
+
+//you have to complete this function
+#include <vector>
+#include <algorithm>
+void quickSort(struct node **headRef)
+{
+    vector<int>vec;
+    node* temp=*headRef;
+    while (temp)
+    {
+        vec.push_back(temp->data);
+        temp=temp->next;
+    }
+    temp=*headRef;
+    sort(vec.begin(), vec.end());
+    for (int i=0;i<vec.size();i++)
+    {
+        temp->data=vec[i];
+        temp=temp->next;
+    }
+}
