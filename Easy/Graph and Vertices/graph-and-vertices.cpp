c8867639bd4ef889f10304f80ae98d652fcb111e
Saransh Bangar <saranshbangad@gmail.com>
2024-01-11 09:24:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Graph and Vertices/graph-and-vertices.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long count(int n)
+    {
+        long long ans=pow(2, (n*(n-1))/2);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.count(n) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
