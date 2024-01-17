658d1b18419291551e6df9052cec3f4cbe30d6bc
Saransh Bangar <saranshbangad@gmail.com>
2024-01-17 09:20:00 +0530

    Added Solution - GfG to GitHub

---
+++ School/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+        string ans="";
+        for (int i=0;i<s.length();i++)
+        {
+            if (s[i]!=' ')
+                ans+=s[i];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
