a9d92838b229e28fe9023210a914f3e3f25a0d1b
Saransh Bangar <saranshbangad@gmail.com>
2024-01-27 09:39:16 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Unique Binary Tree Requirements/unique-binary-tree-requirements.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    bool isPossible(int a,int b)
+    {
+        if (a==b)
+            return false;
+        if (a==2 || b==2)
+            return true;
+        else return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b; 
+        cin>>a>>b;
+        Solution obj;
+        bool ans=obj.isPossible(a,b);
+        cout<<ans<<"\n";
+    }
+}
+// } Driver Code Ends
