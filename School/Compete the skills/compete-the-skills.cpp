f5165305f5c11a7d4fbc991b4ae8e8de611ed349
Saransh Bangar <saranshbangad@gmail.com>
2024-02-08 21:11:08 +0530

    Added Solution - GfG to GitHub

---
+++ School/Compete the skills/compete-the-skills.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    void scores(long long a[], long long b[], int &ca, int &cb)
+    {
+        for (int i=0;i<3;i++)
+        {
+            if (a[i]>b[i])
+                ca++;
+            if (a[i]<b[i])
+                cb++;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t=0;
+    cin>>t;
+    while(t--)
+    {
+        long long int a[5], b[5], i=0;
+        int ca=0, cb=0;
+        for(i=0; i<3; i++)
+            cin>>a[i];
+    
+        for(i=0; i<3; i++)
+            cin>>b[i];
+        Solution ob;
+        ob.scores(a, b, ca, cb);
+        
+        cout<<ca<<" "<<cb<<endl;
+    }
+    return 0 ;
+}
+// } Driver Code Ends
