70c07b2f6e1f8759439911d265f19557d5e07e5d
Saransh Bangar <saranshbangad@gmail.com>
2024-02-08 20:56:12 +0530

    Added Solution - GfG to GitHub

---
+++ School/C++ 2-D Arrays | Set-1/c-2d-arrays-set1.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define M 101
+
+vector<vector<int>> transpose(int a[][M], int n);
+
+int main()
+{
+    int t;
+    cin>>t;
+    int a[M][M];
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>a[i][j];
+            }
+        }
+        vector<vector<int>> b;
+        b = transpose(a, n);
+        for(int i=0;i < n;i++)
+        {
+            for(int j=0;j<n;j++){
+                cout << b[i][j] << " ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+vector<vector<int>>transpose(int a[][M], int n)
+{
+    vector<vector<int>>ans;
+    for (int i=0;i<n;i++)
+    {
+        vector<int>vec;
+        for (int j=0;j<n;j++)
+            vec.push_back(a[j][i]);
+        ans.push_back(vec);
+    }
+    return ans;
+}
