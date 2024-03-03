d4ab9d17656ee7169094aa90433bf139e0b12601
Saransh Bangar <saranshbangad@gmail.com>
2024-03-03 11:24:27 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Professor and Parties/professor-and-parties.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    
+    string PartyType(long long int a[], int n)
+    {
+        sort(a, a+n);
+        for (int i=0;i<n-1;i++)
+        {
+            if (a[i]==a[i+1])
+                return "BOYS";
+        }
+        return "GIRLS";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+
+        int n;
+        cin >> n;
+        long long int a[n+5];
+        for(int i =0;i<n;i++)
+            cin >> a[i];
+        Solution ob;
+        cout<< ob.PartyType(a, n) <<endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
