66cae0dfe9837b7e9dcb1bc931b47a5ee54a9a97
Saransh Bangar <saranshbangad@gmail.com>
2024-03-20 22:15:06 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Segregate even and odd nodes in a Linked List/segregate-even-and-odd-nodes-in-a-linked-list.cpp
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+public:
+    Node* divide(int N, Node *head)
+    {
+        vector<int>odd, even;
+        Node* temp=head;
+        while (temp)
+        {
+            if (temp->data%2==0)
+                even.push_back(temp->data);
+            else odd.push_back(temp->data);
+            temp=temp->next;
+        }
+        temp=head;
+        for (int i=0;i<even.size();i++)
+        {
+            temp->data=even[i];
+            temp=temp->next;
+        }
+        for (int i=0;i<odd.size();i++)
+        {
+            temp->data=odd[i];
+            temp=temp->next;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        Node *ans = ob.divide(N, head);
+        printList(ans); 
+    }
+    return 0;
+}
+
+// } Driver Code Ends
