62ad4109c1dd335ae759b8b107c308edbf97f780
Saransh Bangar <saranshbangad@gmail.com>
2024-03-23 06:57:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Delete a node from BST/delete-a-node-from-bst.cpp
@@ -0,0 +1,153 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+struct Node* deleteNode(struct Node* root, int key);
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void inorder(Node* root, vector<int>& v) {
+    if (root == NULL) return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+int main() {
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root1 = buildTree(s);
+        getline(cin, s);
+        int k = stoi(s);
+        root1 = deleteNode(root1, k);
+        vector<int> v;
+        inorder(root1, v);
+        for (auto i : v) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+// Function to delete a node from BST.
+vector<int>vec;
+void func(Node* root)
+{
+    if (root)
+    {
+        func(root->left);
+        vec.push_back(root->data);
+        func(root->right);
+    }
+}
+Node *deleteNode(Node *root, int X)
+{
+    vec.clear();
+    func(root);
+    if (vec.empty())
+        return nullptr;
+    for (int i=0;i<vec.size();i++)
+    {
+        if (vec[i]==X)
+        {
+            vec[i]=INT_MIN;
+            break;
+        }
+    }
+    Node* head=new Node(vec[0]);
+    if (vec[0]==INT_MIN)
+    {
+        if (vec.size()>1)
+            head->data=vec[1];
+        else return nullptr;
+    }
+    Node* temp=head;
+    for (int i=1;i<vec.size();i++)
+    {
+        if (vec[i]==INT_MIN)
+            continue;
+        Node* newNode=new Node(vec[i]);
+        temp->right=newNode;
+        temp=temp->right;
+    }
+    return head;
+}
