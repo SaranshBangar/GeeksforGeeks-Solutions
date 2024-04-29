1db5e042c0585163872282580585fa952e247b5a
Saransh Bangar <saranshbangad@gmail.com>
2024-04-29 07:46:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) 
+    {
+        if (head == nullptr)
+            return nullptr;
+    
+        unordered_set<int>seen;
+        Node* current=head;
+        Node* prev=nullptr;
+        while (current!=nullptr)
+        {
+            if (seen.find(current->data)!=seen.end())
+            {
+                prev->next=current->next;
+                delete current;
+                current=prev->next;
+            }
+            else
+            {
+                seen.insert(current->data);
+                prev=current;
+                current=current->next;
+            }
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	    Solution ob;
+		Node *result  = ob.removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}
+// } Driver Code Ends
