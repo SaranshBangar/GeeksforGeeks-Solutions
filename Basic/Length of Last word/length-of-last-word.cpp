407361f89b957ad3644c2545199285eefab61d8f
Saransh Bangar <saranshbangad@gmail.com>
2024-05-30 17:26:00 +0530

    Added Solution - GfG to GitHub

--- Basic/Length of Last word/length-of-last-word.cpp
+++ Basic/Length of Last word/length-of-last-word.cpp
@@ -34,7 +34,8 @@ int main()
 
 int findLength(string s)
 {
-    int count=0, len=s.length();
+    int count=0;
+    int len=s.length();
     for (int i=len-1;i>=0;i--)
     {
         if (s[i]==' ' && count==0)
