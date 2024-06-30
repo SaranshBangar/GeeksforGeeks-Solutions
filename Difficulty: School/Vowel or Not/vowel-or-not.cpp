6c048538ff99d936cf060beefbbf57bf4b60bf13
Saransh Bangar <saranshbangad@gmail.com>
2024-06-30 09:31:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Vowel or Not/vowel-or-not.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string isVowel(char c)
+    {
+        if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+            return "YES";
+        return "NO";
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        char c;
+        cin >> c;
+        Solution ob;
+        cout << ob.isVowel(c) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
