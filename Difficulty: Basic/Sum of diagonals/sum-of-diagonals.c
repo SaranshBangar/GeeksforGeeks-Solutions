ab6c1a26d47e2b8649f7b597977664efc457996e
Saransh Bangar <saranshbangad@gmail.com>
2024-06-30 09:36:25 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of diagonals/sum-of-diagonals.c
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C
+
+// } Driver Code Ends
+//User function Template for C
+
+int sumDiagonal(int N, int M[][N])
+{
+    int sum=0;
+    for (int i=0;i<N;i++)
+        sum+=M[i][i];
+    return sum;
+}
+
+
+//{ Driver Code Starts.
+
+#include <stdio.h>
+
+int main() {
+
+	int t;
+	scanf("%d", &t);
+	
+	while(t--)
+	{
+	    int N;
+	    scanf("%d", &N);
+	    
+	    int M[N][N];
+	    for(int i = 0; i < N; i++)
+	    {
+	        for(int j = 0; j < N; j++)
+	        scanf("%d", &M[i][j]);
+	    }
+	    
+	    printf("%d", sumDiagonal(N, M));
+	    printf("\n");
+	}
+	return 0;
+}
+// } Driver Code Ends
