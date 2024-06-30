d952bb03ceed29af78156df973bcbb6af2ff07b4
Saransh Bangar <saranshbangad@gmail.com>
2024-06-30 09:23:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Simple Interest/simple-interest.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    double simpleInterest(int P, int R, int T) 
+    {
+        double p=P, r=R, t=T;
+        double ans=(p*r*t)/100;
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int P, R, T;
+        cin >> P >> R >> T;
+        Solution ob;
+        cout << fixed << setprecision(2);
+        cout << ob.simpleInterest(P, R, T) << "\n";
+    }
+}
+
+// } Driver Code Ends
