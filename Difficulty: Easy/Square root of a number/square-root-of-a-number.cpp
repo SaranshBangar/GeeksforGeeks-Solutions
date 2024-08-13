ad5acdd73f57ef9cb95a3910337adfbb9b96dc9c
Saransh Bangar <saranshbangad@gmail.com>
2024-08-13 11:21:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square root of a number/square-root-of-a-number.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution {
+  public:
+    long long int floorSqrt(long long int n)
+    {
+        long long int i=1, check=1;
+        while (check<=n)
+        {
+              i++;
+              check=i*i;
+        }
+        return i-1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        Solution obj;
+        cout << obj.floorSqrt(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
