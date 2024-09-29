0013dd74385dbf035b5f26a8e7ba701125067586
Saransh Bangar <saranshbangad@gmail.com>
2024-09-29 08:15:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Total count/total-count.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int totalCount(int k, vector<int>& arr) 
+    {
+        int count=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            int q=arr[i]/k;
+            int r=arr[i]%k;
+            count+=q;
+            if(r>0)
+                count++;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.totalCount(k, arr);
+        cout << res << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
