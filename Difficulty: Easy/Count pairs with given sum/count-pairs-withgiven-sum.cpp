af875504501f28390a2040a5b8a7aac970b514fd
Saransh Bangar <saranshbangad@gmail.com>
2024-12-27 19:04:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count pairs with given sum/count-pairs-withgiven-sum.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target)
+    {
+        unordered_map<int, int>mp;
+        int ans=0;
+        for (int num : arr)
+        {
+            int cmp=target-num;
+            if (mp.find(cmp)!=mp.end())
+                ans+=mp[cmp];
+            mp[num]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int target;
+        cin >> target;
+        cin.ignore();
+        Solution ob;
+        int res = ob.countPairs(arr, target);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
