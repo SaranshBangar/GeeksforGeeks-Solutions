be79cc8ac4ea4aa1126f9d003fd5e58200cc2c9d
Saransh Bangar <saranshbangad@gmail.com>
2025-01-03 21:15:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k)
+    {
+        unordered_map<int, int>mp;
+        int prexor=0, cnt=0;
+        for (int num:arr)
+        {
+            prexor^=num;
+            if (prexor==k)
+                cnt++;
+            if (mp.find(prexor^k)!=mp.end())
+                cnt+=mp[prexor^k];
+            mp[prexor]++;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore(); // Ignore the newline after the number of test cases
+
+    while (tc-- > 0) {
+        string input;
+        getline(cin, input); // Read the whole line for the array
+
+        stringstream ss(input);
+        vector<int> arr;
+        int num;
+
+        while (ss >> num) {
+            arr.push_back(num); // Push numbers into the vector
+        }
+
+        int k;
+        cin >> k;
+        cin.ignore(); // Ignore the newline after k
+
+        Solution obj;
+        cout << obj.subarrayXor(arr, k) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
