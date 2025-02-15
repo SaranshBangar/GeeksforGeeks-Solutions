9e299a0f57aacaa1d7c452e6c7d90bad579a46c6
Saransh Bangar <saranshbangad@gmail.com>
2025-02-15 22:13:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Add two numbers/add-two-numbers.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+
+int main() 
+{ 
+    int t; cin>>t;
+    
+    while(t--){
+        int x;int y;
+        cin>>x>>y;
+        
+        
+// } Driver Code Ends
+//User function Template for C++
+
+//complete auto add 
+auto add = [](int x,int y)
+{
+    return (x+y);
+};
+
+
+//{ Driver Code Starts.
+        
+        cout<<add(x,y)<<endl;
+    
+cout << "~" << "\n";
+}
+  
+} 
+// } Driver Code Ends
