04a96e5ad6650f1673191a0c92c194588d2fa6f0
Saransh Bangar <saranshbangad@gmail.com>
2025-02-18 20:41:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
+    {
+        sort(points.begin(), points.end(), [&](vector<int>& A, vector<int>& B)
+        {
+            return (A[0]*A[0]+A[1]*A[1])<(B[0]*B[0]+B[1]*B[1]);
+        });
+        return vector<vector<int>>(points.begin(), points.begin()+k);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int k;
+        cin >> k;
+        int n;
+        cin >> n;
+        vector<vector<int>> points(n, vector<int>(2));
+        for (int i = 0; i < n; i++) {
+            cin >> points[i][0] >> points[i][1];
+        }
+        Solution ob;
+        vector<vector<int>> ans = ob.kClosest(points, k);
+        sort(ans.begin(), ans.end());
+        for (const vector<int>& point : ans) {
+            cout << point[0] << " " << point[1] << endl;
+        }
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
