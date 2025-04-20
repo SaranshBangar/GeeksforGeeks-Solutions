6e2c3f8eba1b65679bb39c432a831212d629e973
Saransh Bangar <saranshbangad@gmail.com>
2025-04-20 10:40:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print adjacency list/print-adjacency-list.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<pair<int, int>>& edges)
+    {
+        vector<vector<int>>adj(V);
+        for(int i=0;i<edges.size();i++)
+        {
+            int u=edges[i].first;
+            int v=edges[i].second;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            
+        }
+        return adj;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<pair<int, int>> edges;
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            edges.push_back({u, v});
+        }
+        Solution obj;
+        vector<vector<int>> adj = obj.printGraph(V, edges);
+        bool empty = true;
+        for (int i = 0; i < V; i++) {
+            if (adj[i].empty())
+                continue;
+            empty = false;
+            break;
+        }
+        if (empty) {
+            cout << "[]\n";
+            continue;
+        }
+        for (int i = 0; i < V; i++) {
+            set<int> st(adj[i].begin(), adj[i].end());
+            cout << "[";
+            auto it = st.begin();
+
+            while (it != st.end()) {
+                cout << *it;
+                if (next(it) != st.end())
+                    cout << " ";
+                ++it;
+            }
+            cout << "]";
+            cout << endl;
+        }
+    }
+}
+// } Driver Code Ends
