732fa235728b621a998bd3b24e74a18d1ca7554f
Saransh Bangar <saranshbangad@gmail.com>
2025-05-04 14:41:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Address Operator/address-operator.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    int a;
+        cin >> a;
+        int *address;
+	    
+	    
+// } Driver Code Ends
+
+// User function Template for C++
+
+// int a;
+// cin >> a;
+// int *address;
+
+// we have to store address of 'a' below
+address = &a;
+
+    // cout << *address << endl;
+
+
+//{ Driver Code Starts.
+	    
+	    cout << *address << endl;
+	
+	    
+	
+cout << "~" << "\n";
+}
+	
+	
+	
+	return 0;
+}
+// } Driver Code Ends
