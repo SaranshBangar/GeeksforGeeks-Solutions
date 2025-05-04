5d77135dcb971bbdebd554ff6f9215fdfb314445
Saransh Bangar <saranshbangad@gmail.com>
2025-05-04 14:27:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Bitwise Right Shift/bitwise-right-shift.cpp
+++ Difficulty: Basic/Bitwise Right Shift/bitwise-right-shift.cpp
@@ -1,16 +1,34 @@
 //{ Driver Code Starts
+// Initial Template for C++
 
-// } Driver Code Ends
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int a, b;
-    cin>>a>>b;
-    cout<<(a>>b);
-    return 0;
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+void rightShift(int a, int b)
+{
+    int ans=a>>b;
+    cout<<ans<<endl;
 }
+
 
 //{ Driver Code Starts.
 
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        rightShift(a, b);
+
+        cout << "~"
+             << "\n";
+    }
+}
 // } Driver Code Ends
