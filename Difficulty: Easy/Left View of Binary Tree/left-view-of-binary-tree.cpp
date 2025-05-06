7e18131b796b2da58cec5a8bd655e6a71f20cf9d
Saransh Bangar <saranshbangad@gmail.com>
2025-05-06 21:26:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
@@ -3,18 +3,67 @@
 using namespace std;
 
 // Tree Node
-struct Node
-{
+struct Node {
     int data;
     Node* left;
     Node* right;
 };
 
-vector<int> leftView(struct Node *root);
+
+// } Driver Code Ends
+
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution {
+  public:
+    vector<int> leftView(Node *root)
+    {
+        vector<vector<int>>ans;
+        vector<int>answer;
+        if (root==nullptr)
+            return answer;
+        queue<Node*>q;
+        q.push(root);
+        while (!q.empty())
+        {
+            int size=q.size();
+            vector<int>level;
+            for (int i=0;i<size;i++)
+            {
+                Node* node=q.front();
+                q.pop();
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+                level.push_back(node->data);
+            }
+            ans.push_back(level);
+        }
+        for (int i=0;i<ans.size();i++)
+            answer.push_back(ans[i][0]);
+        return answer;
+    }
+};
+
+
+//{ Driver Code Starts.
 
 // Utility function to create a new Tree Node
-Node* newNode(int val)
-{
+Node* newNode(int val) {
     Node* temp = new Node;
     temp->data = val;
     temp->left = NULL;
@@ -23,12 +72,10 @@ Node* newNode(int val)
     return temp;
 }
 
-
 // Function to Build Tree
-Node* buildTree(string str)
-{
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
+    if (str.length() == 0 || str[0] == 'N')
         return NULL;
 
     // Creating vector of strings from input
@@ -36,7 +83,7 @@ Node* buildTree(string str)
     vector<string> ip;
 
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
 
     // for(string i:ip)
@@ -51,7 +98,7 @@ Node* buildTree(string str)
 
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
+    while (!queue.empty() && i < ip.size()) {
 
         // Get and remove the front of the queue
         Node* currNode = queue.front();
@@ -61,7 +108,7 @@ Node* buildTree(string str)
         string currVal = ip[i];
 
         // If the left child is not null
-        if(currVal != "N") {
+        if (currVal != "N") {
 
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
@@ -72,12 +119,12 @@ Node* buildTree(string str)
 
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
 
         // If the right child is not null
-        if(currVal != "N") {
+        if (currVal != "N") {
 
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
@@ -91,72 +138,26 @@ Node* buildTree(string str)
     return root;
 }
 
-
 int main() {
     int t;
-    scanf("%d ",&t);
-    while(t--)
-    {
+    scanf("%d ", &t);
+    while (t--) {
         string s;
-        getline(cin,s);
+        getline(cin, s);
         Node* root = buildTree(s);
-        vector<int> vec = leftView(root);
-        for(int x : vec)
-        cout<<x<<" ";
+        Solution ob;
+        vector<int> vec = ob.leftView(root);
+        if (vec.size() == 0) {
+            cout << "[]";
+        }
+        for (int x : vec)
+            cout << x << " ";
         cout << endl;
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
 
-
 // } Driver Code Ends
-
-
-
-
-/* A binary tree node
-
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
-};
- */
-
-//Function to return a list containing elements of left view of the binary tree.
-vector<int> leftView(Node *root)
-{
-    vector<vector<int>>ans;
-    vector<int>answer;
-    if (root==nullptr)
-        return answer;
-    queue<Node*>q;
-    q.push(root);
-    while (!q.empty())
-    {
-        int size=q.size();
-        vector<int>level;
-        for (int i=0;i<size;i++)
-        {
-            Node* node=q.front();
-            q.pop();
-            if (node->left)
-                q.push(node->left);
-            if (node->right)
-                q.push(node->right);
-            level.push_back(node->data);
-        }
-        ans.push_back(level);
-    }
-    for (int i=0;i<ans.size();i++)
-    {
-        answer.push_back(ans[i][0]);
-    }
-    return answer;
-}
