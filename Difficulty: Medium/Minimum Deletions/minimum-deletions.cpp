64128e2af4990f753083c64704ecd67626b765c1
Saransh Bangar <saranshbangad@gmail.com>
2025-05-22 08:43:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Deletions/minimum-deletions.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int minDeletions(string s)
+    {
+        int n = s.size();
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s[i - 1] == rev[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int lps = dp[n][n];
+        return n - lps;
+    }
+};
