75c06ae0aaa28945eab2de6cf82792546acd7551
Saransh Bangar <saranshbangad@gmail.com>
2025-06-06 09:58:53 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
@@ -0,0 +1,47 @@
+#define PRIME 101
+
+class Solution {
+  public:
+    vector<int> search(string &pattern, string &text)
+    {
+        vector<int> ans;
+            int h=pattern.length();
+            int n=text.length();
+            int p=1;
+            for (int i=0;i<h-1;i++)
+            {
+                p=(p*26)%PRIME;
+            }
+            int phash=0;
+            int thash=0;
+            for (int i=0;i<h;i++)
+            {
+                phash=((phash*26)+pattern[i])%PRIME;
+                thash=((thash*26)+text[i]) % PRIME;
+            }
+            for (int i=0;i<=n-h;i++)
+            {
+                if (phash==thash)
+                {
+                    bool match=true;
+                    for (int j=0;j<h;j++)
+                    {
+                        if (pattern[j]!=text[i+j])
+                        {
+                            match=false;
+                            break;
+                        }
+                    }
+                    if (match)
+                        ans.push_back(i+1);
+                }
+                if (i<n-h)
+                {
+                    thash=(26*(thash-text[i]*p)+text[i+h])%PRIME;
+                    if (thash<0)
+                        thash+=PRIME;
+                }
+            }
+            return ans;
+    }
+};
