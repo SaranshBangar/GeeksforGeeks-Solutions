e16976f775fc969c74517b45a5aaf125d8b1cb3f
Saransh Bangar <saranshbangad@gmail.com>
2025-06-25 10:24:38 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check if frequencies can be equal/check-if-frequencies-can-be-equal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    bool sameFreq(string& s)
+    {
+        unordered_map<char,int> mm;
+	    for(auto x:s)mm[x]++;
+	    int mn=INT_MAX,mx=INT_MIN,mxCount=0,mnCount=0;
+	    for(auto x:mm)
+	    {
+	        mn=min(mn,x.second);
+	        mx=max(mx,x.second);
+	    }
+	    for(auto x:mm)
+	    {
+	        if(x.second==mx)mxCount++;
+	        else if(x.second==mn)mnCount++;
+	    }
+	    if(mn==mx or (mn==mx-1 and mxCount==1))return true;
+	    if(mnCount==1 and mn==1 and mxCount+mnCount==mm.size())return true;
+	    return false;
+    }
+};
