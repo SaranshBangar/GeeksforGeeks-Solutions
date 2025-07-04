a5cc53eacd42b67e9a71ccfe8ad5a62562ed8ce5
Saransh Bangar <saranshbangad@gmail.com>
2025-07-04 09:06:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Fizz Buzz/fizz-buzz.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+  public:
+    vector<string> fizzBuzz(int n)
+    {
+        vector<string>vec;
+        for (int i=1;i<=n;i++)
+        {
+            if (i%3==0)
+            {
+                if (i%5==0)
+                    vec.push_back("FizzBuzz");
+                else vec.push_back("Fizz");
+            }
+            else if (i%5==0)
+                vec.push_back("Buzz");
+            else vec.push_back(to_string(i));
+        }
+        return vec;
+    }
+};
