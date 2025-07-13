07192c8584f55a093bb929b5d887135db332195d
Saransh Bangar <saranshbangad@gmail.com>
2025-07-13 18:45:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.cpp
+++ Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.cpp
@@ -1,11 +1,5 @@
-// User function Template for C++
-class Solution {
-  public:
-    int sumOfNumbers(int n)
-    {
-        int ans=0;
-        for (int i=1;i<=n;i++)
-            ans+=i;
-        return ans;
-    }
-};
+int nSum(int n)
+{
+    int ans = (n*(n+1))/2;
+    return ans;
+}
