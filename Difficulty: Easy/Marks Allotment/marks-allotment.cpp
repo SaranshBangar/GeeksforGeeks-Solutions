2304b9f17225cfa465873f7f4d29a480241ed390
Saransh Bangar <saranshbangad@gmail.com>
2025-07-13 18:53:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Marks Allotment/marks-allotment.cpp
@@ -0,0 +1,11 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int marks(int a)
+    {
+        if (a>=0 && a<=100)
+            return a;
+        else return -1;
+    }
+};
