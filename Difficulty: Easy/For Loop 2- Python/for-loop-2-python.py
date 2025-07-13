15d433059412331422c46d05c293d738a0279f28
Saransh Bangar <saranshbangad@gmail.com>
2025-07-13 18:54:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/For Loop 2- Python/for-loop-2-python.py
@@ -0,0 +1,5 @@
+def stringJumper(str):
+    for i in range(0, len(str), 2):  ## from 0 to length of str and skip 2
+        print(
+            str[i],
+            end="")  ##printing character and separating characters by nothing
