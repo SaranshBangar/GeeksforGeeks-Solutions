b7e64039585dcbaa53d45fece3727bce6acc9586
Saransh Bangar <saranshbangad@gmail.com>
2025-07-25 18:16:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
@@ -1,14 +1,5 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // arr: input array
-    // Function to find maximum circular subarray sum.
     int func(vector<int>arr, int n)
     {
         int ans=arr[0], mx=arr[0];
@@ -19,9 +10,8 @@ class Solution {
         }
         return ans;
     }
-    int circularSubarraySum(vector<int> &arr)
+    int maxCircularSum(vector<int> &arr)
     {
-        
         int n=arr.size();
         int mxn=func(arr,n);
         if(mxn<0)
@@ -35,32 +25,4 @@ class Solution {
         int mxc=sum+func(arr,n);
         return max(mxn,mxc);
     }
-    
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.circularSubarraySum(arr);
-
-        cout << res << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
