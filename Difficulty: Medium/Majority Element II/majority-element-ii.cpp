ce6afbebc9045d1fcb4be1330e5b7d21c7da3bcf
Saransh Bangar <saranshbangad@gmail.com>
2025-07-26 10:52:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element II/majority-element-ii.cpp
+++ Difficulty: Medium/Majority Element II/majority-element-ii.cpp
@@ -1,12 +1,5 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // Function to find the majority elements in the array
     vector<int> findMajority(vector<int>& arr)
     {
         vector<int>ans;
@@ -23,32 +16,3 @@ class Solution {
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-
-        string s;
-        getline(cin, s);
-        stringstream ss(s);
-        vector<int> nums;
-        int num;
-        while (ss >> num) {
-            nums.push_back(num);
-        }
-        Solution ob;
-        vector<int> ans = ob.findMajority(nums);
-        if (ans.empty()) {
-            cout << "[]";
-        } else {
-            for (auto &i : ans)
-                cout << i << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
