636ba90c6e1973b207a06a1698ac33ace3481293
Saransh Bangar <saranshbangad@gmail.com>
2025-08-10 21:10:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Palindrome SubStrings/palindrome-sub-strings.cpp
@@ -0,0 +1,38 @@
+
+
+class Solution {
+  public:
+    int expandAroundCenter(const string &s, int left, int right){
+      int count = 0;
+      //expand around center while the characters are equal
+      while(left >= 0 && right < s.length() && s[left] == s[right]){
+          if(right - left + 1 >= 2){
+              count++;
+          }
+          left--;
+          right++;
+      }
+      return count;
+  };
+  
+    int countPS(string &s) {
+        // code here
+    int n = s.length();
+    if(n < 2)   return 0;
+    
+    int totalCount = 0;
+    
+    for(int i = 0; i < n; i++){
+        
+        //odd length palindrome
+        totalCount += expandAroundCenter(s, i, i);
+        
+        //even length palindrome
+        totalCount += expandAroundCenter(s, i, i+1);
+    }
+    
+    return totalCount;
+    
+    }
+};
+
