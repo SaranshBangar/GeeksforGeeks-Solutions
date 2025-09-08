5fe11f4e3c7851ebc18eec16e7799cf9994b2a11
Saransh Bangar <saranshbangad@gmail.com>
2025-09-08 06:33:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.cpp
+++ Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.cpp
@@ -1,34 +1,17 @@
-//{ Driver Code Starts
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
+/*
+class Node {
+public:
     int data;
-    struct Node* next;
-    Node(int x) {
+    Node* next;
+
+    Node(int x){
         data = x;
         next = NULL;
     }
 };
-
-
-// } Driver Code Ends
-/* Structure of the linked list node is as
-struct Node 
-{
-    int data;
-    struct Node* next;
-    Node(int x) { data = x;  next = NULL; }
-};
 */
-
-
-class Solution{
+class Solution {
   public:
-    //Function to sort the given linked list using Merge Sort.
     Node* mergeSort(Node* head)
     {
         vector<int>vec;
@@ -48,40 +31,3 @@ class Solution{
         return head;
     }
 };
-
-
-//{ Driver Code Starts.
-
-void printList(Node* node) {
-    while (node != NULL) {
-        printf("%d ", node->data);
-        node = node->next;
-    }
-    printf("\n");
-}
-
-void push(struct Node** head_ref, int new_data) {
-    Node* new_node = new Node(new_data);
-
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
-}
-
-int main() {
-    long test;
-    cin >> test;
-    while (test--) {
-        struct Node* a = NULL;
-        long n, tmp;
-        cin >> n;
-        for (int i = 0; i < n; i++) {
-            cin >> tmp;
-            push(&a, tmp);
-        }
-        Solution obj;
-        a = obj.mergeSort(a);
-        printList(a);
-    }
-    return 0;
-}
-// } Driver Code Ends
