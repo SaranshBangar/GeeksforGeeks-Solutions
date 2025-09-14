0ab8ce2bece25e300ac0b05644fc85ef7f43f494
Saransh Bangar <saranshbangad@gmail.com>
2025-09-14 23:55:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.cpp
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -1,14 +1,3 @@
-//{ Driver Code Starts
-// C++ program to find union of
-// two sorted arrays
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
-/*You are required to complete this method*/
 class Solution {
   public:
     int startStation(vector<int> &gas, vector<int> &cost)
@@ -33,41 +22,3 @@ class Solution {
         return ans;
     }
 };
-
-
-
-//{ Driver Code Starts.
-/* Driver program to test above function */
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-
-    while (t--) {
-
-        vector<int> gas, cost;
-        string input1;
-        getline(cin, input1);
-        stringstream ss(input1);
-        int number1;
-        while (ss >> number1) {
-            gas.push_back(number1);
-        }
-        string input2;
-        getline(cin, input2);
-        stringstream sss(input2);
-        int number2;
-        while (sss >> number2) {
-            cost.push_back(number2);
-        }
-        Solution ob;
-        int ans = ob.startStation(gas, cost);
-
-        cout << ans << endl;
-        cout << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
